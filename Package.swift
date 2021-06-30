b5f542ffe7e6eaaa3450d83fba3b8f04df40002f
ITzTravelInTime <pietrocaruso97@yahoo.it>
2021-06-30 07:07:30 +0200

    version 3.1.0
    - Fixed protocol extension access issue from external modules
    - Added a new protocol called `Copying`  made for objects that needs to create copies of them selfs as separated objects.
    - Switched back to use release packages for the dependancies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", .branch("main")),
-        .package(url: "https://github.com/ITzTravelInTime/Command", .branch("main"))
+        .package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", from: "1.0.1"),
+        .package(url: "https://github.com/ITzTravelInTime/Command", from: "1.1.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/TINURecovery/Protocols.swift
+++ Sources/TINURecovery/Protocols.swift
@@ -16,14 +16,14 @@ public protocol SimulatableDetectable {
     init()
 }
 
-extension SimulatableDetectable{
+public extension SimulatableDetectable{
     
     /**
      Used to simulate the status for debugging purposes.
      
      You can change the `simulatedStatus` value for debugging purposes, but to have more control on it and for actual usage is reccommended (but not necessary) using a subclass of this one, and ovverriding the `simulatedStatus` variable to use it with your own debug variables system.
      */
-    public static var simulatedStatus: T?{
+    static var simulatedStatus: T?{
         return nil
     }
     
@@ -34,11 +34,16 @@ extension SimulatableDetectable{
      
      You should not ovveride this variable.
      */
-    public static var status: T{
+    static var status: T{
         if let status = simulatedStatus{
             return status
         }
         
         return actualStatus
     }
+}
+
+///Standard protocol for objects that creates copies if themselfs.
+public protocol Copying{
+    func copy() -> Self
 }
