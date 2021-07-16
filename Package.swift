1c417fb76e18e582b827388652a9a6dcb21dd910
ITzTravelIntime <23339020+ITzTravelInTime@users.noreply.github.com>
2021-07-16 12:27:12 +0200

    Version 3.4.2
    - Updated platform requirements and dependancies

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "TINURecovery",
     platforms: [
         .macOS("10.9"),
-        .iOS("7.0"),
-        .watchOS(.v2),
-        .tvOS(.v9)
+        .iOS("8.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -22,7 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", from: "1.2.0"),  //no longer used, it was required by the SIP detector but it's no longer needed
         .package(url: "https://github.com/ITzTravelInTime/Command", from: "2.1.0"),
-        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "2.0.1")
+        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "3.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
