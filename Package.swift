3668ac07dfcef092a4e256aa1c43be6b5207ac77
ITzTravelInTime <pietrcaruso50@gmail.com>
2022-06-17 13:51:35 +0200

    Version 5.0.1 - Lowered Swift version requirements. - Lowered iOS Version requirements.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "TINURecovery",
     platforms: [
         .macOS("10.9"),
-        .iOS("8.0")
+        .iOS("7.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -20,8 +20,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         //.package(url: "https://github.com/ITzTravelInTime/SwiftCPUDetect", from: "1.2.0"),  //no longer used, it was required by the SIP detector but it's no longer needed
         //.package(url: "https://github.com/ITzTravelInTime/Command", from: "2.1.0"), //no longer needed because now the SIP stuff is get using the IO Registry
-        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "3.2.0"),
-        .package(url: "https://github.com/ITzTravelInTime/TINUIORegistry", from: "0.1.1"),
+        .package(url: "https://github.com/ITzTravelInTime/SwiftLoggedPrint", from: "3.3.1"),
+        .package(url: "https://github.com/ITzTravelInTime/TINUIORegistry", from: "0.1.3"),
         .package(url: "https://github.com/ITzTravelInTime/SwiftPackagesBase", from: "0.0.14")
     ],
     targets: [
